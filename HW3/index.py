42277b28121c98dceab1fbf9e4e6e4406376f281
Agentum07 <gupta.abhi0710@gmail.com>
2023-03-21 01:40:02 +0800

    Finalize index and search

--- HW3/index.py
+++ HW3/index.py
@@ -9,7 +9,8 @@ from nltk.stem.porter import PorterStemmer
 import time
 import math
 import pickle
-from collections import Counter, defaultdict
+from collections import defaultdict
+import util
 
 def usage():
     print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
@@ -24,13 +25,6 @@ def reset_dir(out_dict, out_postings, out_doc_lengths):
     if os.path.exists(out_doc_lengths):
         os.remove(out_doc_lengths)
 
-def log_tf(term_freq):
-    weight = 0
-    if term_freq > 0:
-        weight = 1 + math.log(term_freq, 10)
-
-    return weight
-
 # commands to run
 # python HW3/index.py -i "" -d HW3/dictionary.txt -p HW3/postings.txt
 def build_index(in_dir, out_dict, out_postings):
@@ -43,8 +37,6 @@ def build_index(in_dir, out_dict, out_postings):
     start_time = time.perf_counter()
 
     print('indexing...')
-    # save the posting list
-    # save the dictionary
     # This is an empty method
     # Pls implement your code in below
     stemmer = PorterStemmer()
@@ -67,76 +59,31 @@ def build_index(in_dir, out_dict, out_postings):
                     word = stemmer.stem(word.lower())
                     # word has not been seen before
                     if word not in posting_list:
-                        # dictionary.add(word)
                         posting_list[word] = {}
 
                     if doc_id not in posting_list[word]:
                         posting_list[word][doc_id] =  1
                     else:
                         posting_list[word][doc_id] +=  1
-        
-        # calulate the length of each doc as explained on piazza
-        doc_length[doc_id] = calculate_doc_length(posting_list, doc_id)
+
+        # calculate the length of each doc as explained on piazza
+        doc_length[doc_id] = util.calculate_doc_length(posting_list, doc_id)
         
     # convert freqs to tf
     for word in posting_list:
         # print(word, posting_list[word])
         for doc_id, term_freq in posting_list[word].items():
-            posting_list[word][doc_id] = log_tf(term_freq)
+            posting_list[word][doc_id] = util.log_tf(term_freq)
 
     # format posting list to be more readable
-    posting_list = format_posting_list(posting_list)
+    posting_list = util.format_posting_list(posting_list)
 
     # save posting list and dictionary
-    save_posting_list_and_dictionary(posting_list, out_postings, out_dict)
-    save_doc_lengths(doc_length, 'doc_lengths.txt')
+    util.save_posting_list_and_dictionary(posting_list, out_postings, out_dict)
+    util.save_doc_lengths(doc_length, 'doc_lengths.txt')
 
     print("Indexed in ({:.2f}s)".format(time.perf_counter() - start_time))
     print('indexing complete.')
-
-
-def save_posting_list_and_dictionary(posting_list, posting_dir, dictionary_dir):
-    dictionary = {}
-    with open(posting_dir, 'wb') as posting_ptr:
-        for word, doc_lst in sorted(posting_list.items()):
-            offset = posting_ptr.tell()
-            dictionary[word] = offset
-            pickle.dump(sorted(doc_lst), posting_ptr)
-    posting_ptr.close()
-
-    with open(dictionary_dir, 'wb') as dict_ptr:
-        pickle.dump(dictionary, dict_ptr)
-    dict_ptr.close()
-
-
-def save_doc_lengths(doc_length, doc_length_dir):
-    with open(doc_length_dir, 'wb') as doc_ptr:
-        pickle.dump(doc_length, doc_ptr)
-    doc_ptr.close()
-    
-
-def calculate_doc_length(posting_list, doc_id):
-    doc_N = 0
-    term_freq = 0
-    for word in posting_list.keys():
-        if doc_id in posting_list[word]:
-            term_freq += math.pow(log_tf(posting_list[word][doc_id]), 2)
-    doc_N = math.sqrt(term_freq)
-    return doc_N
-
-
-def format_posting_list(posting_list):
-    """
-        Formats from dict to optimised storable and readable data strcture
-        {word: {docId: tf}} => [(docId, tf)]
-        :param posting_list: of format {word: {docId: tf}}
-    """
-    final_postings = defaultdict(list)
-    for word in posting_list:
-        for doc_id, tf in posting_list[word].items():
-            final_postings[word].append((doc_id, tf))
-    
-    return final_postings
 
 input_directory = output_file_dictionary = output_file_postings = None
 
--- HW3/search.py
+++ HW3/search.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-import re
-import nltk
 import sys
 import getopt
+from nltk import word_tokenize
+from nltk.stem.porter import PorterStemmer
+from collections import defaultdict
+from math import log, sqrt
+import heapq
+import util
+
+# the number of 
+TOP_K = 10
 
 def usage():
     print("usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results")
@@ -12,10 +19,83 @@ def run_search(dict_file, postings_file, queries_file, results_file):
     using the given dictionary file and postings file,
     perform searching on the given queries file and output the results to a file
     """
+    print("loading dictionary")
+    dictionary = util.load_dict(dict_file)
+    doc_lengths = util.load_doc_lengths('doc_lengths.txt')
+
     print('running search on the queries...')
     # This is an empty method
     # Pls implement your code in below
+    queries = open(queries_file, 'r')
+    results = open(results_file, 'w')
 
+    query_results = []
+    for query in queries:
+        result = evaluate_query(query, dictionary, postings_file, doc_lengths)
+        result = util.format_result(result)
+        query_results.append(result)
+
+    final_text = "\n".join(query_results)
+    results.write(final_text)
+    print('result generated.')
+
+
+def evaluate_query(query, dictionary, postings_file, doc_lengths):
+    '''
+    Evaluates the query to return the top 10 values
+    :param query: the query to be evaluated
+    :param query: the query to be evaluated
+    :param query: the query to be evaluated
+    :param query: the dictionary containing
+    '''
+    stemmer = PorterStemmer()
+    tokens = word_tokenize(query)
+    tokens = [stemmer.stem(token.lower()) for token in tokens]
+
+    tf_query = defaultdict(float)
+    document_score = defaultdict(float)
+    N = len(doc_lengths)
+
+    for token in tokens:
+        tf_query[token] += 1
+    
+    for token in set(tokens):
+        # https://stackoverflow.com/questions/9047364/how-to-check-for-a-key-in-a-defaultdict-without-updating-the-dictionary-python
+        if token not in dictionary:
+            posting_list = []
+        else:
+            offset = dictionary[token]
+            posting_list = util.get_posting_list(postings_file, offset)
+        
+        df = len(posting_list) # number of docs that contain the token
+
+        if df <= 0:
+            idf = 0
+        else:
+            idf = log(N / df, 10)
+
+        tf_query[token] = idf * util.log_tf(tf_query[token])
+
+        for doc_id_tf_pair in posting_list:
+            doc_id, tf = doc_id_tf_pair
+            document_score[doc_id] += tf_query[token] * tf
+
+        # if you make it counter document_score 
+
+        # then normalise document_score 
+        # document_score.most_common(10)
+
+    query_length = 0
+    for freq in tf_query.values():
+        query_length += freq * freq 
+
+    query_length = sqrt(query_length)
+
+    for doc_id, doc_score in document_score.items():
+        document_score[doc_id] = doc_score / (doc_lengths[doc_id] * query_length)
+
+    return heapq.nlargest(TOP_K, document_score, key=document_score.__getitem__)
+    
 dictionary_file = postings_file = file_of_queries = output_file_of_results = None
 
 try:
